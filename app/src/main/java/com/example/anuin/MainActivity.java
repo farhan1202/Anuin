683d494504165e902762d18d7e3882f45e701990
farhan <hafifi1202@gmail.com>
2020-02-17 11:37:41 +0700

    Initial commit

---
+++ app/src/main/java/com/example/anuin/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.anuin;
+
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
+
+
+    }
+
+    private BottomNavigationView.OnNavigationItemReselectedListener listener =
+            new Bottom
