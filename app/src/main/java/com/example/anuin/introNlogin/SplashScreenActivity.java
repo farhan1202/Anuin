c4f62e222911d3f482aa94d0b3548ce97cc675ee
darkyasha14 <shinigaminoirfan@gmail.com>
2020-02-17 22:53:51 +0700

    splashscreen , intro silde, api login

---
+++ app/src/main/java/com/example/anuin/introNlogin/SplashScreenActivity.java
@@ -0,0 +1,42 @@
+package com.example.anuin.introNlogin;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Build;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+
+import com.example.anuin.R;
+
+public class SplashScreenActivity extends AppCompatActivity {
+    private int time=2000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        if (Build.VERSION.SDK_INT >= 21) {
+            getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                    | View.SYSTEM_UI_FLAG_FULLSCREEN);
+        }
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            Window window = getWindow();
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.setStatusBarColor(Color.TRANSPARENT);
+        }
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                Intent home=new Intent(SplashScreenActivity.this, WelcomeActivity.class);
+                startActivity(home);
+                finish();
+
+            }
+        },time);
+    }
+}
