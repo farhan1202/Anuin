597d2f12f3f532d6b00876d4748968bfb54d90a0
farhan <hafifi1202@gmail.com>
2020-02-17 11:55:00 +0700

    menambah fungsi bottom navigation pada aplikasi

--- app/src/main/java/com/example/anuin/MainActivity.java
+++ app/src/main/java/com/example/anuin/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.anuin;
 
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
@@ -14,9 +15,34 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
-
-
+        bottomNav.setOnNavigationItemSelectedListener(listener);
     }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener listener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                    Fragment selecFrag = null;
+                    switch (menuItem.getItemId()) {
+                        case R.id.nav_home:
+                            selecFrag = new HomeFrag();
+                            break;
+                        case R.id.nav_order:
+                            selecFrag = new OrderFrag();
+                            break;
+                        case R.id.nav_profil:
+                            selecFrag = new ProfileFrag();
+                            break;
+                        case R.id.nav_other:
+                            selecFrag = new OtherFrag();
+                            break;
+                    }
+
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selecFrag).commit();
+
+                    return true;
+                }
+            };
 }
 
 
