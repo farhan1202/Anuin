cc3cda798b715c58ad6b9f3e7aa6a4e25d574da5
farhan <hafifi1202@gmail.com>
2020-02-17 16:17:59 +0700

    selesai membuat fungsi move activity ke FormPemesanan

--- app/src/main/java/com/example/anuin/Adapter/OrderModalSheet.java
+++ app/src/main/java/com/example/anuin/Adapter/OrderModalSheet.java
@@ -1,5 +1,6 @@
 package com.example.anuin.Adapter;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -7,14 +8,34 @@ import android.support.design.widget.BottomSheetDialogFragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
+import com.example.anuin.DetailJasaActivity;
+import com.example.anuin.FormPesananActivity;
 import com.example.anuin.R;
 
 public class OrderModalSheet extends BottomSheetDialogFragment {
     @Nullable
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable final ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.modal_bottom_sheet, container, false);
+        Button btnCancel = view.findViewById(R.id.btnCancelOrder);
+        Button btnAccept = view.findViewById(R.id.btnAcceptOrder);
+
+        btnCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dismiss();
+            }
+        });
+
+        btnAccept.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(view.getContext() , FormPesananActivity.class);
+                startActivity(intent);
+            }
+        });
         return view;
 
     }
---
+++ app/src/main/java/com/example/anuin/FormPesananActivity.java
@@ -0,0 +1,13 @@
+package com.example.anuin;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FormPesananActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_form_pesanan);
+    }
+}
