bcc2dc68b08c524d20828d747b9554cc849ff2d7
farhan <hafifi1202@gmail.com>
2020-02-18 16:50:02 +0700

    Selesai mengerjakan layout

--- app/src/main/java/com/example/anuin/DetailJasaActivity.java
+++ app/src/main/java/com/example/anuin/DetailJasaActivity.java
@@ -6,13 +6,15 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.RadioButton;
+import android.widget.Toast;
 
 import com.example.anuin.Adapter.OrderModalSheet;
 
 public class DetailJasaActivity extends AppCompatActivity {
 Button btnPesan;
 Toolbar toolbar;
-
+RadioButton rB1, rB2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +25,13 @@ Toolbar toolbar;
         toolbar = findViewById(R.id.toolbarDetailJasa);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        rB1 = findViewById(R.id.radioPerbaikan);
+        rB2 = findViewById(R.id.radioSurvey);
+
+        if (rB2.isChecked() || rB2.isSelected()){
+            Toast.makeText(this, "asd", Toast.LENGTH_SHORT).show();
+        }
+
 
 
         btnPesan.setOnClickListener(new View.OnClickListener() {
--- app/src/main/java/com/example/anuin/FormPesananActivity.java
+++ app/src/main/java/com/example/anuin/FormPesananActivity.java
@@ -1,11 +1,28 @@
 package com.example.anuin;
 
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.TimePicker;
+import android.widget.Toast;
+
+import java.sql.Time;
+import java.util.Calendar;
 
 public class FormPesananActivity extends AppCompatActivity {
 Toolbar toolbar;
+EditText txtFormDate, txtFormTime;
+Calendar calendar, calender1;
+DatePickerDialog dialog;
+Time time;
+TimePickerDialog dialog1;
+Context mContext = this;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -13,5 +30,41 @@ Toolbar toolbar;
         toolbar = findViewById(R.id.toolbarFormPemesanan);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        txtFormTime = findViewById(R.id.txtFormTime);
+        txtFormDate = findViewById(R.id.txtFormDate);
+        txtFormDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                calendar = Calendar.getInstance();
+                final int day = calendar.get(Calendar.DAY_OF_MONTH);
+                final int month = calendar.get(Calendar.MONTH + 1);
+                final int year = calendar.get(Calendar.YEAR);
+
+                dialog = new DatePickerDialog(FormPesananActivity.this, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker datePicker, int i, int i1, int i2) {
+                        txtFormDate.setText(i2 + "/" + i1 + "/" + i);
+                    }
+                },year, month, day);
+                dialog.show();
+            }
+        });
+
+        txtFormTime.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                calender1 = Calendar.getInstance();
+                final int hour = calender1.get(Calendar.HOUR_OF_DAY);
+                final int minute = calender1.get(Calendar.MINUTE);
+
+                dialog1 = new TimePickerDialog(FormPesananActivity.this, new TimePickerDialog.OnTimeSetListener() {
+                    @Override
+                    public void onTimeSet(TimePicker timePicker, int i, int i1) {
+                        txtFormTime.setText(i + ":" + i1 + " WIB");
+                    }
+                }, hour, minute, android.text.format.DateFormat.is24HourFormat(mContext));
+                dialog1.show();
+            }
+        });
     }
 }
