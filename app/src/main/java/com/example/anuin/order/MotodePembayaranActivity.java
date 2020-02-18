69e5ccc804967cd2dc10d80be916f380e24d068b
darkyasha14 <shinigaminoirfan@gmail.com>
2020-02-18 14:13:51 +0700

    PayActivity

--- app/src/main/java/com/example/anuin/order/MotodePembayaranActivity.java
+++ app/src/main/java/com/example/anuin/order/MotodePembayaranActivity.java
@@ -1,7 +1,10 @@
 package com.example.anuin.order;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
+import android.view.View;
 
 import com.example.anuin.R;
 
@@ -11,5 +14,14 @@ public class MotodePembayaranActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_motode_pembayaran);
+        CardView cvTunai=findViewById(R.id.cvTunai);
+
+        cvTunai.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), BayarActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
