1c6bed144d6b7ad934d67c1119c46c8b0872af77
fadilHidayatul <bangtans46@gmail.com>
2020-04-30 14:57:36 +0700

    Add dependecies to gradle android & gradle project Add file google-services.json to app folder add firebase instance to get token in other activity

--- app/src/main/java/com/example/anuin/other/NotifActivity.java
+++ app/src/main/java/com/example/anuin/other/NotifActivity.java
@@ -1,19 +1,30 @@
 package com.example.anuin.other;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.appcompat.widget.Toolbar;
+
+import android.util.Log;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.example.anuin.Adapter.NotifAdapter;
 import com.example.anuin.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.InstanceIdResult;
 
 public class NotifActivity extends AppCompatActivity {
 RecyclerView rvNotif;
 NotifAdapter adapter;
 Toolbar toolbar;
+String TAG = "TAG";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,6 +39,26 @@ Toolbar toolbar;
         rvNotif.setLayoutManager(new LinearLayoutManager(this));
         adapter = new NotifAdapter();
         rvNotif.setAdapter(adapter);
+
+        getToken();
+    }
+
+    private void getToken() {
+        FirebaseInstanceId.getInstance().getInstanceId().addOnCompleteListener(new OnCompleteListener<InstanceIdResult>() {
+            @Override
+            public void onComplete(@NonNull Task<InstanceIdResult> task) {
+                if (!task.isSuccessful()){
+                    Log.w("getInstanceFailed",task.getException());
+                    return;
+                }
+
+                //get new InstanceID Token
+                String token = task.getResult().getToken();
+//                String getMessage = getString(R.string.msg_token_fmt,token);
+                Log.e(TAG, token);
+                Toast.makeText(NotifActivity.this, token, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     @Override
